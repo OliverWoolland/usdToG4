ae9b7eb23d7300259c5a0ee8ae13d0b3a5c5ead6
Oliver Woolland <oliver.woolland@manchester.ac.uk>
2022-01-18 10:22:12 +0000

    Add command line interface

--- readUSD_makePYG4.py
+++ readUSD_makePYG4.py
@@ -1,42 +1,35 @@
 # ------------------------------------------------------------------------------
 # Libraries
 
-## Pixar
-from pxr import Usd, Vt
+# Pixar
+from pxr import Usd
 
-## General
-import math, os
-import matplotlib.pyplot as plt
-import numpy as np
+# General
+import os
+import argparse
+import sys
 
-## Geant4
+# Geant4
 import pyg4ometry as pyg4
 
-## Local
-import usdHelpers as uh
+# Local
 import pyg4Helpers as pgh
 
 # ------------------------------------------------------------------------------
 # Support functions
 
 # ------------------------------------------------------------------------------
-# Set up USD env
+# Process command line options
 
-#stage = Usd.Stage.Open('../USDExamples/Kitchen_set/assets/Ball/Ball.usd')
-#stage = Usd.Stage.Open('../USDExamples/Kitchen_set/assets/GarlicRope/GarlicRope.usd')
-#stage = Usd.Stage.Open('../USDExamples/Kitchen_set/Kitchen_set.usd')
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('usd_input', type=str, nargs=1,
+                    help='The USD file to convert to GDML')
+args = parser.parse_args()
+print(args.accumulate(args.usd_input))
 
-#stage = Usd.Stage.Open('../USDExamples/omniverse/example_meshAndShape.usd')
-stage = Usd.Stage.Open('../USDExamples/omniverse/example_mesh.usd')
-#stage = Usd.Stage.Open('../USDExamples/omniverse/example_shape.usd')
-#stage = Usd.Stage.Open('../USDExamples/omniverse/example_cubes.usd')
-#stage = Usd.Stage.Open('../USDExamples/omniverse/example_spheres.usd')
-stage = Usd.Stage.Open('../USDExamples/omniverse/cppscript.usd')
-
-#stage = Usd.Stage.Open('../USDExamples/PointInstancedMedCity.usd')
-#stage = Usd.Stage.Open('../USDExamples/UsdSkelExamples/HumanFemale/HumanFemale.usd')
-
-#stage = Usd.Stage.Open('../USDExamples/Reactor/octomak4.usd')
+stage_file = args.usd_input
+sys.exit()
+stage = Usd.Stage.Open(f'{stage_file}')
 
 # ------------------------------------------------------------------------------
 # Set up G4 env
@@ -45,15 +38,15 @@ stage = Usd.Stage.Open('../USDExamples/omniverse/cppscript.usd')
 registry = pyg4.geant4.Registry()
 
 # world solid and logical
-worldSolid  = pyg4.geant4.solid.Box( "worldSolid", # name
-                                     100000,         # x size
-                                     100000,         # y size
-                                     100000,         # z size
-                                     registry )     # registry
-worldLogical = pyg4.geant4.LogicalVolume( worldSolid,     # solid volume
-                                          "G4_Galactic",  # material
-                                          "worldLogical", # name
-                                          registry )      # registry
+worldSolid  = pyg4.geant4.solid.Box("worldSolid",  # name
+                                    100000,        # x size
+                                    100000,        # y size
+                                    100000,        # z size
+                                    registry)      # registry
+worldLogical = pyg4.geant4.LogicalVolume(worldSolid,      # solid volume
+                                         "G4_Galactic",   # material
+                                         "worldLogical",  # name
+                                         registry)        # registry
 registry.setWorld(worldLogical.name)
 
 # ------------------------------------------------------------------------------
@@ -106,32 +99,3 @@ viewer.addLogicalVolume(worldLogical)
 #viewer.addAxes(20)
 #viewer.setWireframe()
 viewer.view()
-
-# ------------------------------------------------------------------------------
-# Read GDML file
-
-# import xml.etree.ElementTree as ET
-# tree = ET.parse('test.gdml')
-# root = tree.getroot()
-
-# fig = plt.figure()
-# ax = fig.add_subplot(projection='3d')
-
-# x = []
-# y = []
-# z = []
-
-# for el in root.iter('position'):
-#     ex = float(el.attrib.get('x'))
-#     ey = float(el.attrib.get('y'))
-#     ez = float(el.attrib.get('z'))
-
-#     x.append(ex)
-#     y.append(ey)
-#     z.append(ez)
-
-#     ax.scatter( ex,
-#                 ey,
-#                 ez,
-#                 marker='o'
-#                )
